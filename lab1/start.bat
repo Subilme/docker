16514acd120c3c4dd188381b4fc40e16a01dc18d
Subilme <leukhin95@list.ru>
2023-12-26 20:37:42 +0300

    added versioning

--- lab1/start.bat
+++ lab1/start.bat
@@ -1,4 +1,4 @@
-docker build -t container .
+docker build -t container:1.0 .
 
-docker run -d -p 1234:8080 --name first container
-docker run -d -p 5678:8080 --name second container
+docker run -d -p 1234:8080 --name first container:1.0
+docker run -d -p 5678:8080 --name second container:1.0
